ebfcc59df8fdcbf45ec488e6d0da4730b67620f4
Valeriy <valeriy.em95@mail.ru>
2023-10-30 19:16:00 +0600

    review2

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -10,11 +10,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAuthorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.method in
-            permissions.SAFE_METHODS or
-            request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -173,5 +173,3 @@ DJOSER = {
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
